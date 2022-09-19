cbbf3a41e65639a9a961452899b5851de4116e7a
ryomaemura <ryomaemura34@gmail.com>
2022-09-19 17:19:44 +0900

    add minesweeper

--- Assets/Scenes/NumberManager.cs
+++ Assets/Scenes/NumberManager.cs
@@ -24,7 +24,6 @@ public class NumberManager : MonoBehaviour
     [SerializeField] Button button15;
     [SerializeField] Button button16;
     Button[] buttons;
-    int[] selectButtons = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
     [SerializeField] TextMeshProUGUI buttonText1;
     [SerializeField] TextMeshProUGUI buttonText2;
     [SerializeField] TextMeshProUGUI buttonText3;
@@ -42,209 +41,87 @@ public class NumberManager : MonoBehaviour
     [SerializeField] TextMeshProUGUI buttonText15;
     [SerializeField] TextMeshProUGUI buttonText16;
     TextMeshProUGUI[] buttonTexts;
-    [SerializeField] Button maxButton;
-    [SerializeField] Button minButton;
-    [SerializeField] Button sumButton;
-    Button[] modeButtons;
-    [SerializeField] TextMeshProUGUI moveText;
-    int moveNumber = 0;
-    [SerializeField] TextMeshProUGUI resultText;
-    int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16};
-    // mode : max, min, sum
-    int[] modeBoolean = {0, 0, 0};
-    int modeFlag = 0;
-    int[] addNumbers = {0, 1, 4, 5};
-    int maxNumber = 0;
-    int minNumber = 100;
-    int sumNumber = 0;
-    [SerializeField] TMP_InputField numberInputField;
-    [SerializeField] Button submitButton;
-    [SerializeField] TextMeshProUGUI descriptionText;
-    int inputNumber = 0;
-    int setFlag = 0;
-    int randomNumber1 = 0;
-    int randomNumber2 = 0;
-    int temp = 0;
+    [SerializeField] TextMeshProUGUI bombText;
+    [SerializeField] TextMeshProUGUI gameOverText;
+    int[] numbers = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+    int bombNumber = 3;
+    int randomNumber = 0;
+    int[,] relasionshipNumbers = {
+        {0, 1, 0, 0,   1, 1, 0, 0,   0, 0, 0, 0,   0, 0, 0, 0},
+        {1, 0, 1, 0,   1, 1, 1, 0,   0, 0, 0, 0,   0, 0, 0, 0},
+        {0, 1, 0, 1,   0, 1, 1, 1,   0, 0, 0, 0,   0, 0, 0, 0},
+        {0, 0, 1, 0,   0, 0, 1, 1,   0, 0, 0, 0,   0, 0, 0, 0},
+
+        {1, 1, 0, 0,   0, 1, 0, 0,   1, 1, 0, 0,   0, 0, 0, 0},
+        {1, 1, 1, 0,   1, 0, 1, 0,   1, 1, 1, 0,   0, 0, 0, 0},
+        {0, 1, 1, 1,   0, 1, 0, 1,   0, 1, 1, 1,   0, 0, 0, 0},
+        {0, 0, 1, 1,   0, 0, 1, 0,   0, 0, 1, 1,   0, 0, 0, 0},
+
+        {0, 0, 0, 0,   1, 1, 0, 0,   0, 1, 0, 0,   1, 1, 0, 0},
+        {0, 0, 0, 0,   1, 1, 1, 0,   1, 0, 1, 0,   1, 1, 1, 0},
+        {0, 0, 0, 0,   0, 1, 1, 1,   0, 1, 0, 1,   0, 1, 1, 1},
+        {0, 0, 0, 0,   0, 0, 1, 1,   0, 0, 1, 0,   0, 0, 1, 1},
+
+        {0, 0, 0, 0,   0, 0, 0, 0,   1, 1, 0, 0,   0, 1, 0, 0},
+        {0, 0, 0, 0,   0, 0, 0, 0,   1, 1, 1, 0,   1, 0, 1, 0},
+        {0, 0, 0, 0,   0, 0, 0, 0,   0, 1, 1, 1,   0, 1, 0, 1},
+        {0, 0, 0, 0,   0, 0, 0, 0,   0, 0, 1, 1,   0, 0, 1, 0},
+    };
+    int relasionshipBombsNumber = 0;
 
     // Start is called before the first frame update
     void Start() {
         buttons = new Button[] {button1, button2, button3, button4, button5, button6, button7, button8, button9, button10, button11, button12, button13, button14, button15, button16};
         buttonTexts = new TextMeshProUGUI[] {buttonText1, buttonText2, buttonText3, buttonText4, buttonText5, buttonText6, buttonText7, buttonText8, buttonText9, buttonText10, buttonText11, buttonText12, buttonText13, buttonText14, buttonText15, buttonText16};
-        modeButtons = new Button[] {maxButton, minButton, sumButton};
 
-        resetnumbers();
-        resetNumberInputField();
-        resetSubmitButton();
+        resetBomb();
     }
 
     // Update is called once per frame
     void Update() {
     }
 
-    public void resetnumbers() {
-        for (int i = 0; i < 20; i++) {
+    public void resetBomb() {
+        for (int i = 0; i < numbers.Length; i++) {
+            numbers[i] = 0;
+            buttons[i].GetComponent<Image>().color = new Color32(100, 0, 200, 255);
+            buttonTexts[i].text = "";
+        }
+
+        for (int i = 0; i < bombNumber; i++) {
             // output 0 ~ numbers.length - 1
-            randomNumber1 = UnityEngine.Random.Range(0, numbers.Length);
-            // output 0 ~ numbers.length - 1
-            randomNumber2 = UnityEngine.Random.Range(0, numbers.Length);
+            randomNumber = UnityEngine.Random.Range(0, numbers.Length);
 
-            temp = numbers[randomNumber1];
-            numbers[randomNumber1] = numbers[randomNumber2];
-            numbers[randomNumber2] = temp;
+            if (numbers[randomNumber] != 1) {
+                numbers[randomNumber] = 1;
+            } else {
+                i--;
+            }
         }
-    }
 
-    public void resetNumberButton() {
-        for (int i = 0; i < buttons.Length; i++) {
-            buttons[i].GetComponent<Image>().color = new Color32(255, 255, 255, 255);
-            selectButtons[i] = 0;
-        }
-    }
-
-    public void resetModeButton() {
-        for (int i = 0; i < modeBoolean.Length; i++) {
-            modeBoolean[i] = 0;
-            modeButtons[i].GetComponent<Image>().color = new Color32(255, 255, 255, 255);
-        }
-    }
-
-    public void resetNumberInputField() {
-        numberInputField.GetComponent<Image>().color = new Color32(100, 100, 100, 255);
-    }
-
-    public void resetSubmitButton() {
-        submitButton.GetComponent<Image>().color = new Color32(100, 100, 100, 255);
-    }
-
-    public void clickModeButton(int modeNumber) {
-        if (modeBoolean[modeNumber] == 0) {
-            resetNumberButton();
-            resetModeButton();
-            resetNumberInputField();
-            resetSubmitButton();
-
-            modeBoolean[modeNumber] = 1;
-            modeButtons[modeNumber].GetComponent<Image>().color = new Color32(255, 100, 100, 255);
-        } else {
-            resetNumberButton();
-            resetModeButton();
-            resetNumberInputField();
-            resetSubmitButton();
-        }
+        bombText.text = "Bomb:" + bombNumber.ToString();
+        gameOverText.text = "";
     }
 
     public void clickNumberButton(int buttonNumber) {
-        resetNumberInputField();
-        resetSubmitButton();
+        relasionshipBombsNumber = 0;
 
-        modeFlag = 0;
-        for (int i = 0; i < modeBoolean.Length; i++) {
-            if (modeBoolean[i] == 1) {
-                modeFlag = 1;
-                selectNumber(buttonNumber);
-            }
-        }
+        if (numbers[buttonNumber] == 0) {
+            // not bomb
+            buttons[buttonNumber].GetComponent<Image>().color = new Color32(255, 255, 255, 255);
 
-        if (modeFlag == 0) {
-            resetNumberButton();
-
-            selectButtons[buttonNumber] = 1;
-            buttons[buttonNumber].GetComponent<Image>().color = new Color32(0, 200, 255, 255);
-            numberInputField.GetComponent<Image>().color = new Color32(255, 255, 255, 255);
-            submitButton.GetComponent<Image>().color = new Color32(255, 255, 255, 255);
-        }
-    }
-
-    public void selectNumber(int buttonNumber) {
-        if (selectButtons[buttonNumber] == 1) {
-            resetNumberButton();
-        } else if (
-            buttonNumber != 3 &
-            buttonNumber != 7 &
-            buttonNumber != 11 &
-            buttonNumber != 12 &
-            buttonNumber != 13 &
-            buttonNumber != 14 &
-            buttonNumber != 15
-        ) {
-            resetNumberButton();
-
-            for (int i = 0; i < addNumbers.Length; i++) {
-                buttons[buttonNumber + addNumbers[i]].GetComponent<Image>().color = new Color32(255, 100, 100, 255);
-                selectButtons[buttonNumber + addNumbers[i]] = 1;
-            }
-        }
-    }
-
-    public void setNumber() {
-        setFlag = 0;
-        int.TryParse(numberInputField.text, out inputNumber);
-
-        for (int i = 0; i < selectButtons.Length; i++) {
-            if (selectButtons[i] == 1 && numbers[i] == inputNumber) {
-                setFlag = 1;
-
-                buttonTexts[i].text = inputNumber.ToString();
-                descriptionText.text = "Correct answer";
-
-                resetNumberButton();
-                resetNumberInputField();
-                resetSubmitButton();
-            }
-        }
-
-        if (setFlag == 0) {
-            descriptionText.text = "Wrong number";
-        }
-    }
-
-    public void okButton() {
-        if (modeBoolean[0] == 1) {
-            // max mode
-            maxNumber = 0;
-
-            for (int i = 0; i < selectButtons.Length; i++) {
-                if (selectButtons[i] == 1 & maxNumber < numbers[i]) {
-                    maxNumber = numbers[i];
+            for (int i = 0; i < numbers.Length; i++) {
+                if (relasionshipNumbers[buttonNumber, i] == 1 && numbers[i] == 1) {
+                    relasionshipBombsNumber++;
                 }
             }
 
-            showResult(maxNumber);
-        } else if (modeBoolean[1] == 1) {
-            // min mode
-            minNumber = 100;
-
-            for (int i = 0; i < selectButtons.Length; i++) {
-                if (selectButtons[i] == 1 & minNumber > numbers[i]) {
-                    minNumber = numbers[i];
-                }
-            }
-
-            showResult(minNumber);
+            buttonTexts[buttonNumber].text = relasionshipBombsNumber.ToString();
         } else {
-            // sum mode
-            sumNumber = 0;
+            // bomb
+            buttons[buttonNumber].GetComponent<Image>().color = new Color32(255, 0, 0, 255);
 
-            for (int i = 0; i < selectButtons.Length; i++) {
-                if (selectButtons[i] == 1) {
-                    sumNumber = sumNumber + numbers[i];
-                }
-            }
-
-            showResult(sumNumber);
+            gameOverText.text = "Game Over";
         }
-    }
-
-    public void showResult(int resultNumber) {
-        if (modeBoolean[0] == 1) {
-            resultText.text = "Max : " + resultNumber.ToString();
-        } else if (modeBoolean[1] == 1) {
-            resultText.text = "Min : " + resultNumber.ToString();
-        } else {
-            resultText.text = "Sum : " + resultNumber.ToString();
-        }
-
-        moveNumber = moveNumber + 1;
-        moveText.text = "Move : " + moveNumber.ToString();
     }
 }
